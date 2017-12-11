16613c9b6dfcbdb97af35020524c192513fbed08
shmethod <sr0478@pleasantonusd.net>
2017-12-11 10:00:26 -0800

    start

---
+++ IrregularPolygon.java
@@ -0,0 +1,18 @@
+import java.awt.geom.*;    
+import java.util.ArrayList; 
+import gpdraw.*; 
+public class IrregularPolygon{
+   private ArrayList <Point2D.Double> myPolygon;
+
+   // constructors
+   public IrregularPolygon() { }
+
+   // public methods
+   public void add(Point2D.Double aPoint) { }
+
+   public void draw() { }
+
+   public double perimeter() { }
+
+   public double area() { }
+}
